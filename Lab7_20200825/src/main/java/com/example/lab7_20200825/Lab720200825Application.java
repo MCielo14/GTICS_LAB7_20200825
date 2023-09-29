706796255c55a08cab6a21f079d1b7b3f5a65186
= <=>
2023-09-28 20:38:30 -0500

    inicio del lab7 gtics

---
+++ Lab7_20200825/src/main/java/com/example/lab7_20200825/Lab720200825Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20200825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720200825Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720200825Application.class, args);
+    }
+
+}
---
+++ Lab7_20200825/src/test/java/com/example/lab7_20200825/Lab720200825ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20200825;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720200825ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
